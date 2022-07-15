36e75bc878fa5f9e4a83842014ff2465eefa390c
Aidan-2020 <69286745+Aidan-2020@users.noreply.github.com>
2022-07-15 15:00:01 -0400

    dash
    dash finally works

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float walkSpeed = 5f;
+    private float moveSpeed = 5f;
 
     public Rigidbody2D rb;
 
     Vector2 movement;
+
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    private int direction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dashTime = startDashTime;
     }
 
     // Update is called once per frame
@@ -20,6 +26,55 @@ public class playerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+
+        if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            //moving le
+            direction = 1;
+        }
+        else if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            //moving right
+            direction = 2;
+        }
+        else if (Input.GetAxisRaw("Vertical") == 1)
+        {
+            direction = 3;
+        }
+        else if (Input.GetAxisRaw("Vertical") == -1)
+        {
+            direction = 4;
+        }
+
+        if (dashTime <= 0)
+        {
+            direction = 0;
+            dashTime = startDashTime;
+            moveSpeed = walkSpeed;
+        }
+        else
+        {
+            dashTime -= Time.deltaTime;
+            if (direction == 1 && Input.GetKeyDown(KeyCode.Space))
+            {
+
+                moveSpeed = dashSpeed;
+                print("try to dash left");
+            }
+            else if (direction == 2 && Input.GetKeyDown(KeyCode.Space))
+            {
+                moveSpeed = dashSpeed;
+            }
+            else if (direction == 3 && Input.GetKeyDown(KeyCode.Space))
+            {
+                moveSpeed = dashSpeed;
+            }
+            else if (direction == 4 && Input.GetKeyDown(KeyCode.Space))
+            {
+                moveSpeed = dashSpeed;
+            }
+        }
     }
 
     private void FixedUpdate()
