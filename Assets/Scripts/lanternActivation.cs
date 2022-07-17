64d185204d5f480afa280c074a5d19a411164906
Aidan-2020 <69286745+Aidan-2020@users.noreply.github.com>
2022-07-16 23:12:13 -0400

    added more tutorial setup

--- Assets/Scripts/lanternActivation.cs
+++ Assets/Scripts/lanternActivation.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class lanternActivation : MonoBehaviour
 {
+    public Transform textBox;
+
+    public Transform anchor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        textBox.position = new Vector3(textBox.position.x, 5000f, textBox.position.z);
     }
 
     // Update is called once per frame
@@ -18,7 +21,12 @@ public class lanternActivation : MonoBehaviour
 
     public void bringDownText()
     {
+        textBox.position = new Vector3(textBox.position.x, anchor.position.y, textBox.position.z);
+    }
 
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(4);
     }
 
 }
