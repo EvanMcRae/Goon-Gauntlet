29e610b1dd25605929f260563a6326cdb60e0897
EvanMcRae <evanmmcrae@gmail.com>
2022-07-17 12:21:52 -0400

    trapdoor sounds

--- Assets/Scripts/trap.cs
+++ Assets/Scripts/trap.cs
@@ -8,10 +8,13 @@ public class trap : MonoBehaviour
     public bool killMode = false;
     private IEnumerator coru;
     private bool waiting = false;
+    private AudioSource[] sources;
+    public AudioClip stepSound, openSound, closeSound;
 
     // Start is called before the first frame update
     void Start()
     {
+        sources = transform.GetComponents<AudioSource>();
         coru = Wait();
     }
 
@@ -73,16 +76,41 @@ public class trap : MonoBehaviour
 
     IEnumerator Wait()
     {
+        PlaySound(stepSound);
         waiting = true;
         //print("waiting?");
         yield return new WaitForSeconds(2f);
+        PlaySound(openSound);
         sr.enabled = false;
         sr2.enabled = true;
         killMode = true;
         yield return new WaitForSeconds(4f);
+        PlaySound(closeSound);
         sr.enabled = true;
         sr2.enabled = false;
         killMode = false;
         waiting = false;
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        foreach (AudioSource source in sources)
+        {
+            if (source.clip == clip && source.isPlaying)
+            {
+                if (source.time < 0.2f && source.isPlaying) return;
+                else source.Stop();
+            }
+        }
+        for (int index = sources.Length - 1; index >= 0; index--)
+        {
+            if (!sources[index].isPlaying)
+            {
+                sources[index].clip = clip;
+                sources[index].loop = false;
+                sources[index].Play();
+                return;
+            }
+        }
     }
 }
